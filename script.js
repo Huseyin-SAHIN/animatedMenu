9fabef7734c0afe9df16aecd8635771f04fbe45b
Huseyin SAHIN <sakarya.hsa54@gmail.com>
2023-03-07 16:52:07 +0300

    firstCommit

---
+++ script.js
@@ -0,0 +1,7 @@
+const circle = document.querySelector(".circle")
+const container = document.querySelector(".container")
+
+
+circle.addEventListener("click", () => {
+    container.classList.toggle("show-nav");
+})
